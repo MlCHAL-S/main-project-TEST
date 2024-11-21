44943f47c9a92ac5f4c064194aa632fe783480a1
Michal <michal.szynkaruk@wp.pl>
2024-11-21 20:24:02 +0100

    Adding CI

--- service/__init__.py
+++ service/__init__.py
@@ -6,7 +6,7 @@ from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config.DevelopmentConfig)
+    app.config.from_object(config.TestingConfig)
 
     db.init_app(app)
 
