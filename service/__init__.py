6aaca7b319b042b0989fb462eb9156d53f6a8ea4
Michal <michal.szynkaruk@wp.pl>
2024-11-22 19:57:20 +0100

    Started adding Talisman and CORS but I guess I will finish that at the end

--- service/__init__.py
+++ service/__init__.py
@@ -1,15 +1,17 @@
-from service import config
+from service.config import config_by_name
 from flask import Flask
-from .extensions import db
+from .extensions import db, talisman, cors
 from .routes import main
 
 
-def create_app():
+def create_app(config_name='development'):
 
     app = Flask(__name__)
-    app.config.from_object(config.TestingConfig)
+    app.config.from_object(config_by_name[config_name])
 
     db.init_app(app)
+    talisman.init_app(app)
+    cors.init_app(app)
 
     with app.app_context():
         db.create_all()
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,18 @@ import pytest
 from service import create_app, db
 from service.extensions import redis_client
 from unittest.mock import Mock
+from service.extensions import talisman
 
 
 @pytest.fixture()
 def app():
-    app = create_app()
+    app = create_app(config_name='testing')
 
     redis_client.get = Mock(return_value=None)  # Mock `get` to return `None`
     redis_client.set = Mock()  # Mock `set` to do nothing
     redis_client.delete = Mock()
 
-
+    talisman.force_https = False
 
     yield app
 
